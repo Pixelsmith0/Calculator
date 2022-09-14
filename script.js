30d56f6e2aa4f73557a28c6e0307d009d9bb1c71
M <mfetterly91@gmail.com>
2022-09-14 09:34:12 -0700

    Added button functionality. Modified operate() to work with buttons.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let storedValues = [];
+let workingValues = [];
+let storedOperator = 0;
+
 function addNums(arr) {
     const numArr = arr;
     return numArr.reduce((total, ele) => total += ele);
@@ -18,15 +22,62 @@ function divideNums(arr) {
     return numArr.reduce((total, ele) => total /= ele);
 };
 
-function operate(...args) {
-    const nums = args.filter(arg => typeof arg === 'number')
-    if (args.includes('+')) {
-        return addNums(nums);
-    } else if (args.includes('-')) {
-        return subtractNums(nums);
-    } else if (args.includes('*')) {
-        return multiplyNums(nums);
-    } else if (args.includes('/')) {
-        return divideNums(nums);
+// If storedValues is empty, it becomes equal to workingValues. Otherwise checks the value of storedOperator, pushes
+// workingValue to StoredValue, and runs the appropriate fn.
+function operate() {
+    if (!storedValues[0]) {
+        storedValues = workingValues;
+    } else if (storedOperator === '+') {
+        storedValues.push(workingValues[0]);
+        storedValues = [addNums(storedValues)];
+    } else if (storedOperator === '-') {
+        storedValues.push(workingValues[0]);
+        storedValues = [subtractNums(storedValues)];
+    } else if (storedOperator === 'x') {
+        storedValues.push(workingValues[0]);
+        storedValues = [multiplyNums(storedValues)];
+    } else if (storedOperator === '/') {
+        storedValues.push(workingValues[0]);
+        storedValues = [divideNums(storedValues)];
     };
 };
+
+const displayScreen = document.querySelector('#mainDisplay');
+const previousScreen = document.querySelector('#previousDisplay');
+const numButtons = document.querySelectorAll('.numButton');
+numButtons.forEach(button => button.addEventListener('click', function(e) {
+    displayScreen.innerText += `${button.innerText}`;
+    workingValues.push(button.innerText);
+    workingValues = [parseInt(workingValues.join(''))];
+}));
+
+const operatorButtons = document.querySelectorAll('.operatorButton'); 
+// On click, if workingValues is empty, does nothing. 
+// Otherwise runs operate() and adds the button's innertext to displayScreen, changes the storedOperator to the 
+// last one clicked, and empties workingValues.
+operatorButtons.forEach(button => button.addEventListener('click', function(e) {
+    if (typeof workingValues[0] !== 'number') return;
+    operate();
+    displayScreen.innerText += `${button.innerText}`;
+    storedOperator = button.innerText;
+    workingValues = [];
+}));
+
+const equalButton = document.querySelector('#equalsbtn');
+equalButton.addEventListener('click', function(e) {
+    if (typeof workingValues[0] !== 'number') return;
+    displayScreen.innerText += `${this.innerText}`;
+    operate();
+    previousScreen.innerText = displayScreen.innerText;
+    displayScreen.innerText = storedValues;
+    workingValues = storedValues;
+    storedValues = [];
+});
+
+const clearButton = document.querySelector('#clearbtn');
+clearButton.addEventListener('click', function(e) {
+    storedValues = [];
+    workingValues = [];
+    displayScreen.innerText = '';
+    previousScreen.innerText = '';
+});
